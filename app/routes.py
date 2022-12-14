7be056ce911377d4209c2b9892c0fa10b5454eab
IWasAced <kilianyeokaixi@gmail.com>
2022-12-15 00:17:09 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from flask import Flask,request,flash,render_template,make_response,redirect,url_for,session
-from flask_login import LoginManager,login_required,logout_user,current_user,login_user
+from flask import Flask, request, flash, render_template, make_response, redirect, url_for, session
+from flask_login import LoginManager, login_required, logout_user, current_user, login_user
 from flask_sqlalchemy import SQLAlchemy
 from app.Forms import *
 from app.models import User
@@ -17,23 +17,28 @@ with app.app_context():
     users = User.query.all()
     print(users)
     if User.query.filter_by(username="admin").first():
-    # The "admin" username already exists, so handle the error
-    # ...
+        # The "admin" username already exists, so handle the error
+        # ...
         print("admin exist")
         pass
     else:
         # The "admin" username does not exist, so create a new user
-        user=User(username="admin",password_hash="pbkdf2:sha256:150000$o3FaFCJz$c2b91aa2abd9eac0480aaf06861061c8ba6f7d75167dc03acea09dddf47eeac0",is_active=True,role="Administrator")
+        user = User(username="admin",
+                    password_hash="pbkdf2:sha256:150000$o3FaFCJz$c2b91aa2abd9eac0480aaf06861061c8ba6f7d75167dc03acea09dddf47eeac0",
+                    is_active=True, role="Administrator")
         db.session.add(user)
         db.session.commit()
-        
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -50,6 +55,7 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     create_user_form = CreateUserForm(request.form)
@@ -58,7 +64,7 @@ def register():
     if request.method == 'POST':
         username = request.form['first_name']
         password = request.form['password']
-        user = User(username=username,password_hash=password,is_active=True,role="User")
+        user = User(username=username, password_hash=password, is_active=True, role="User")
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
@@ -66,11 +72,12 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=create_user_form)
 
+
 @app.route('/retrieving')
 def retrieving_users():
     if not session.get('type'):
         session['type'] = 'guest'
-    if current_user.is_authenticated :
+    if current_user.is_authenticated:
         if current_user.has_role('superuser'):  # edited to prevent path traversal
             response = make_response(render_template('retrieving.html'))
             return response
